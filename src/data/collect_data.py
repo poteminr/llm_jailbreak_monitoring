8a5f3cab1fa7340c4e33a1df0b557a31a7bcaca7
favead <diablo3_monk@mail.ru>
2024-09-06 17:55:15 +0300

    Added dataset collecting logic

---
+++ src/data/collect_data.py
@@ -0,0 +1,81 @@
+import json
+import os
+from typing import Dict, List, Union
+
+import click
+from datasets import Dataset
+from github import Auth, Github
+
+from processer import DataProcesser, NotFoundDataError
+
+
+def collect_recursively_from_github(
+    g: Github, repo_path: str, initial_path: str, data_processer: DataProcesser
+) -> List[Dict[str, Union[str, bool]]]:
+    repo = g.get_repo(repo_path)
+    contents = repo.get_contents(initial_path)
+    data = []
+
+    def check_extentions(download_url: str) -> bool:
+        ext = download_url.split(".")[-1]
+        if ext in ["csv", "json"]:
+            return True
+        return False
+
+    while contents:
+        conent_file = contents.pop(0)
+        if conent_file.type == "dir":
+            contents.extend(repo.get_contents(conent_file.path))
+        else:
+            if check_extentions(conent_file.download_url):
+                try:
+                    print(f"Try to process {conent_file.download_url} data")
+                    data += data_processer.process_filedata(repo_path, conent_file)
+                except NotFoundDataError:
+                    continue
+    return data
+
+
+def create_huggingface_dataset(
+    data: List[Dict[str, Union[str, bool]]],
+    local_dataset_path: str,
+    hf_dataset_path: str,
+) -> None:
+    dataset = Dataset.from_list(data)
+    dataset.save_to_disk(local_dataset_path)
+    dataset.push_to_hub(repo_id=hf_dataset_path, private=True)
+    return None
+
+
+@click.command()
+@click.option("--output_path", type=click.STRING)
+@click.optin("--local_dataset_path", type=click.STRING)
+@click.option("--hf_dataset_path", type=click.STRING)
+def collect(output_path: str, local_dataset_path: str, hf_dataset_path: str) -> None:
+    all_data = []
+    auth = Auth.Token(os.getenv("GITHUB_TOKEN"))
+    g = Github(auth=auth)
+    data_processer = DataProcesser()
+
+    if os.path.exists(output_path):
+        with open(output_path, "r") as f:
+            all_data = json.load(f)
+        all_data = all_data["data"]
+    else:
+        with open("git_repos.json", "r") as f:
+            git_repos = json.load(f)
+
+        for git_repo in git_repos["repos"]:
+            all_data += collect_recursively_from_github(
+                g, git_repo["repo_path"], git_repo["initial_path"], data_processer
+            )
+
+        with open(output_path, "w") as f:
+            json.dump({"data": all_data}, f)
+
+    create_huggingface_dataset(all_data, local_dataset_path, hf_dataset_path)
+    return None
+
+
+if __name__ == "__main__":
+    collect()
---
+++ src/data/processer.py
@@ -0,0 +1,85 @@
+import json
+from typing import Dict, List, Union
+import urllib
+
+from github import ContentFile
+import numpy as np
+import pandas as pd
+import requests
+
+
+class NotFoundDataError(Exception):
+    pass
+
+
+class DataProcesser:
+    def __init__(self) -> None:
+        self.verazuo = "verazuo/jailbreak_llms"
+        self.jailbreakbench = "JailbreakBench/artifacts"
+        self.sherdencooper = "sherdencooper/GPTFuzz"
+        self.tml_epfl = "tml-epfl/llm-adaptive-attacks"
+
+        self.target_field = "jailbreak"
+        self.verazuo_fields = ["prompt", "jailbreak"]
+        self.jailbreakbench_fields = ["prompt", "jailbroken"]
+        self.sherdencooper_fields = ["text"]
+
+    def _encode_field(self, key: str) -> str:
+        if key == "jailbroken":
+            return self.target_field
+        return key
+
+    def _process_sherdencooper_filedata(
+        self, content_file: ContentFile
+    ) -> List[Dict[str, Union[str, bool]]]:
+        df = pd.read_csv(content_file.download_url)
+        df = df[self.sherdencooper_fields]
+        df[self.target_field] = np.ones(df.shape[0]).astype(bool)
+        return df.to_dict(orient="records")
+
+    def _process_verazuo_filedata(
+        self, content_file: ContentFile
+    ) -> List[Dict[str, Union[str, bool]]]:
+        try:
+            df = pd.read_csv(content_file.download_url)
+        except urllib.error.URLError:
+            response = requests.get(content_file.download_url)
+            if response.status_code == 200:
+                with open("local_file.csv", "wb") as f:
+                    f.write(response.content)
+                df = pd.read_csv("local_file.csv")
+            else:
+                print(f"Failed to download the file: {response.status_code}")
+        df = df[self.verazuo_fields]
+        return df.to_dict(orient="records")
+
+    def _process_jailbreakbench_filedata(
+        self, content_file: ContentFile
+    ) -> List[Dict[str, Union[str, bool]]]:
+        data = json.loads(content_file.decoded_content)
+        jailbreak_data = data.get("jailbreaks", None)
+        if jailbreak_data:
+            data = [
+                {
+                    self._encode_field(k): v
+                    for k, v in jailbreak_item.items()
+                    if k in self.jailbreakbench_fields
+                }
+                for jailbreak_item in jailbreak_data
+            ]
+            return data
+        else:
+            raise NotFoundDataError
+
+    def process_filedata(
+        self, source_repo: str, content_file: ContentFile
+    ) -> List[Dict[str, Union[bool, str]]]:
+        match source_repo:
+            case self.verazuo:
+                return self._process_verazuo_filedata(content_file)
+            case self.jailbreakbench:
+                return self._process_jailbreakbench_filedata(content_file)
+            case self.sherdencooper:
+                return self._process_sherdencooper_filedata(content_file)
+            case self.tml_epfl:
+                return self._process_jailbreakbench_filedata(content_file)
