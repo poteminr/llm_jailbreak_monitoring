88d35887f5b61341ded0dafa3a1497654ca95887
favead <diablo3_monk@mail.ru>
2024-09-10 14:19:32 +0300

    Added functionality for merging jailbreak input datasets

--- src/data/merge_data.py
+++ src/data/merge_data.py
@@ -1,24 +1,31 @@
 import json
+from typing import Any, Dict
 
 import click
 import datasets
+from datasets import Value
 
-from data.processer import HfDataProcesser
-from data.utils import process_dataset, remove_columns, rename_columns
+from processer import HfDataProcesser
+from utils import process_dataset, remove_columns, rename_columns
 
 
 @click.command()
-@click.option("--local_dataset_path", type=click.STRING)
+@click.option("--output_local_dataset_path", type=click.STRING)
 @click.option("--output_hf_dataset_path", type=click.STRING)
+@click.option("--jailbreak_local_dataset_path", type=click.STRING)
 @click.option("--jailbreak_hf_dataset_path", type=click.STRING)
 def merge_data(
-    local_dataset_path: str, output_hf_dataset_path: str, jailbreak_hf_dataset_path: str
+    output_local_dataset_path: str,
+    output_hf_dataset_path: str,
+    jailbreak_local_dataset_path: str,
+    jailbreak_hf_dataset_path: str,
 ) -> None:
     """
     Merge datasets from Hugging Face and local storage.
 
-    :param local_dataset_path: Path to the local dataset.
+    :param output_local_dataset_path: Path to the local output dataset.
     :param output_hf_dataset_path: Path to the output dataset on Hugging Face.
+    :param jailbreak_local_dataset_path: Path to the local jailbreak dataset
     :param jailbreak_hf_dataset_path: Path to the jailbreak dataset on Hugging Face.
     """
     to_merge_output = []
@@ -34,7 +41,8 @@ def merge_data(
         output_field = hf_dataset["output_field"]
         label_field = hf_dataset["label_field"]
         jailbreak_field = hf_dataset["jailbreak_field"]
-        dataset = datasets.load_dataset(dataset_path)
+        dataset_name = hf_dataset["dataset_name"]
+        dataset = datasets.load_dataset(dataset_path, name=dataset_name)
 
         for split in dataset.keys():
             split_dataset = dataset[split]
@@ -45,10 +53,22 @@ def merge_data(
 
             if jailbreak_field in split_dataset.column_names:
                 # Обработка jailbreak запросов
-                jailbreak_dataset = split_dataset.filter(
-                    lambda item: hf_processer.is_jailbreak(
+
+                def cast_to_int(item: Dict[str, Any]) -> Dict[str, Any]:
+                    item[jailbreak_field] = hf_processer.transform_jailbreak_field(
                         dataset_path, item[jailbreak_field]
                     )
+                    return item
+
+                jailbreak_dataset = split_dataset.filter(
+                    lambda item: bool(
+                        hf_processer.transform_jailbreak_field(
+                            dataset_path, item[jailbreak_field]
+                        )
+                    )
+                )
+                jailbreak_dataset = jailbreak_dataset.map(
+                    lambda item: cast_to_int(item)
                 )
                 jailbreak_dataset = remove_columns(
                     jailbreak_dataset, [input_field, jailbreak_field]
@@ -58,7 +78,11 @@ def merge_data(
                     input_field=input_field,
                     jailbreak_field=jailbreak_field,
                 )
-                to_merge_jailbreak.extend(jailbreak_dataset)
+
+                new_features = jailbreak_dataset.features.copy()
+                new_features["jailbreak"] = Value("int64")
+                jailbreak_dataset = jailbreak_dataset.cast(new_features)
+                to_merge_jailbreak.append(jailbreak_dataset)
 
             if output_field in split_dataset.column_names:
                 # Обработка генерации модели
@@ -72,10 +96,12 @@ def merge_data(
 
     if to_merge_jailbreak:
         initial_jaiblreak_dataset = datasets.load_dataset(jailbreak_hf_dataset_path)
-        to_merge_jailbreak.extend(initial_jaiblreak_dataset)
+        to_merge_jailbreak.append(initial_jaiblreak_dataset["train"])
 
-    process_dataset(to_merge_output, local_dataset_path, output_hf_dataset_path)
-    process_dataset(to_merge_jailbreak, local_dataset_path, jailbreak_hf_dataset_path)
+    process_dataset(to_merge_output, output_local_dataset_path, output_hf_dataset_path)
+    process_dataset(
+        to_merge_jailbreak, jailbreak_local_dataset_path, jailbreak_hf_dataset_path
+    )
 
     return None
 
