689f58697f32036a8f89ca5cd97ff4c1ba3f21a7
nikiduki <72929274+nikiduki@users.noreply.github.com>
2024-09-11 23:52:59 +0300

    Update vectorize_df.py
    Добавлен аргумент column_name

--- rag/vectorize_df.py
+++ rag/vectorize_df.py
@@ -2,13 +2,15 @@ from sentence_transformers import SentenceTransformer
 import pandas as pd
 
 
-def vectorize_df(data: pd.DataFrame, 
+def vectorize_df(data: pd.DataFrame,
+                 column_name: str,
                  model_name: str = "intfloat/multilingual-e5-large",
                  device: str = "cuda"
                  ) -> pd.DataFrame:
     """
     Vectorize dataframe and return dataframe with "embedding".
     :param data: dataframe with strings.
+    :param column_name: name of column to be vectorized.
     :param device: name of the device where the model will be run.
     :return: vectorized data stored in dataframe["embedding"].
     """
@@ -16,5 +18,5 @@ def vectorize_df(data: pd.DataFrame,
                                           device=device)
     
     vectorized_data = pd.DataFrame(columns=["embedding"])
-    vectorized_data["embedding"] = embedding_model.encode(data).tolist()
+    vectorized_data["embedding"] = embedding_model.encode(data[column_name]).tolist()
     return vectorized_data
