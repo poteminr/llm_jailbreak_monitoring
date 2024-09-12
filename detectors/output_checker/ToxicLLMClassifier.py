a61a52077f2bff9683bb6c61355b4956b2e0a7a9
nikiduki <72929274+nikiduki@users.noreply.github.com>
2024-09-12 18:24:59 +0300

    Update ToxicLLMClassifier.py
    Описание класса ToxicLLMClassifier

--- detectors/output_checker/ToxicLLMClassifier.py
+++ detectors/output_checker/ToxicLLMClassifier.py
@@ -1,8 +1,7 @@
 import torch
 import torch.nn as nn
-from transformers import DistilBertTokenizer, DistilBertModel
+from transformers import DistilBertModel
 from huggingface_hub import PyTorchModelHubMixin
-from typing import Optional
 
 
 class ToxicLLMClassifier(nn.Module, 
@@ -28,37 +27,3 @@ class ToxicLLMClassifier(nn.Module,
             outputs = self(input_ids, attention_mask)
             _, preds = torch.max(outputs, dim=1)
         return preds
-
-def infer(text: str,
-          model: Optional[ToxicLLMClassifier] = ToxicLLMClassifier.from_pretrained("nikiduki/ToxicLLMClassifier"),
-          tokenizer: Optional[DistilBertTokenizer] = DistilBertTokenizer.from_pretrained("distilbert-base-uncased", clean_up_tokenization_spaces=True),
-          device: str = "cuda"
-          )-> bool:
-    """
-    Model inference function that determines whether the text is toxic or not.
-    :param model_name: name of classification model.
-    :param tokenizer_name: name of tokenizer.
-    :param device: name of the device where the model will be run.
-    :return: boolean value of model toxicity.
-    """
-    
-    model.to(device)
-    model.eval()
-    encoding = tokenizer.encode_plus(
-        text,
-        add_special_tokens=True,
-        max_length=128,
-        return_token_type_ids=False,
-        padding='max_length',
-        truncation=True,
-        return_attention_mask=True,
-        return_tensors='pt',
-    )
-    
-    input_ids = encoding['input_ids'].to(device)
-    attention_mask = encoding['attention_mask'].to(device)
-    
-    with torch.no_grad():
-        output = model.predict(input_ids, attention_mask)
-    
-    return output.item()
