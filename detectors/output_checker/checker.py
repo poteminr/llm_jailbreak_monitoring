0590c2023c91a10f6f796ce82fdbc9c89aec4c8e
nikiduki <72929274+nikiduki@users.noreply.github.com>
2024-09-12 21:03:40 +0300

    Create ToxicLLMClassifier.py (#7)
    * Create ToxicLLMClassifier.py
    
    Добавлена реализация энкодер модели для классификации токсичности генерации LLM-сервиса.
    
    * Update ToxicLLMClassifier.py
    
    Описание класса ToxicLLMClassifier
    
    * Update checker.py
    
    Добавлена реализация ToxicLLMClassifier в checker.py
    
    * Rename ToxicLLMClassifier.py to toxic_classifier.py
    
    Переименование модуля в toxic_classifier.py
    
    * set n_classes
    
    ---------
    
    Co-authored-by: Roman Potemin <poteminr@gmail.com>

--- detectors/output_checker/checker.py
+++ detectors/output_checker/checker.py
@@ -1,13 +1,39 @@
 from typing import Optional
 import ollama
+from transformers import DistilBertTokenizer
+from toxic_classifier import ToxicLLMClassifier
 
 
 class OutputDetector:
-    def __init__(self, llm_guard_model_name: str = "xe/llamaguard3") -> None:
+    def __init__(self, llm_guard_model_name: str = "xe/llamaguard3", encoder_model_name: str = "nikiduki/ToxicLLMClassifier", tokenizer_name: str = "distilbert-base-uncased", device: str = "cuda") -> None:
         self.llm_guard_model_name = llm_guard_model_name
+        self.device = device
+        self.encoder_model = ToxicLLMClassifier.from_pretrained(self.encoder_model_name).to(self.device),
+        self.tokenizer = DistilBertTokenizer.from_pretrained(self.tokenizer_name, clean_up_tokenization_spaces=True),
     
     def _generate(self, prompt: str, system: str = "") -> str:
         return ollama.generate(self.llm_guard_model_name, prompt=prompt, system=system)['response']
+
+    def _inference(self, text: str,) -> str:
+        self.encoder_model.eval()
+        encoding = self.tokenizer.encode_plus(
+            text,
+            add_special_tokens=True,
+            max_length=128,
+            return_token_type_ids=False,
+            padding='max_length',
+            truncation=True,
+            return_attention_mask=True,
+            return_tensors='pt',
+        )
+
+        input_ids = encoding['input_ids'].to(self.device)
+        attention_mask = encoding['attention_mask'].to(self.device)
+    
+        with torch.no_grad():
+            output = self.encoder_model.predict(input_ids, attention_mask)
+    
+        return output.item()
     
     @staticmethod
     def get_parse_llm_guard_output(model_output: str) -> dict[str, Optional[str]]:
@@ -20,9 +46,24 @@ class OutputDetector:
             result['label'] = False
             
         return result
+
+    @staticmethod
+    def get_encoder_output(model_output: int) -> dict[str, Optional[str]]:
+        result = {}
+        if model_output != 0:
+            result['category'] = str(model_output)
+            result['label'] = True
+        else:
+            result['category'] = None
+            result['label'] = False
+            
+        return result
     
     def get_llm_guard_model_prediction(self, input_text: str) -> dict[str, Optional[str]]:
         return self.get_parse_llm_guard_output(self._generate(input_text))
+
+    def get_encoder_model_prediction(self, input_text: str) -> dict[str, Optional[str]]:
+        return self.get_encoder_output(self._inference(input_text))
         
     
 class OutputChecker:
