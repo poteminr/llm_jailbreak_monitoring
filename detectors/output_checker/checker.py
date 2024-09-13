c6c4d0c07a3194463b0c38d05461e2ec327b1ccd
Roman Potemin <poteminr@gmail.com>
2024-09-13 09:32:11 +0300

    refactoring

--- detectors/output_checker/checker.py
+++ detectors/output_checker/checker.py
@@ -1,7 +1,6 @@
 from typing import Optional, Literal
 import ollama
-import torch
-from transformers import pipeline, AutoModelForSequenceClassification
+from transformers import pipeline
 
 
 class OutputDetector:
